422addd2a20387b0927e74cca3add172e5d4dd5f
rufi156 <rkarpinski@sigma.ug.edu.pl>
2020-01-05 17:25:09 +0100

    Updates

---
+++ precache-manifest.ad4e747b543123df0e22d8beec58fe5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d5da3ea01e4f76fe7196a7e184b125",
+    "url": "/mylmap/index.html"
+  },
+  {
+    "revision": "c76b24f5bb6516bbb8fa",
+    "url": "/mylmap/static/css/2.074064f9.chunk.css"
+  },
+  {
+    "revision": "e04c4dc47e92b046eafe",
+    "url": "/mylmap/static/css/main.bf286928.chunk.css"
+  },
+  {
+    "revision": "c76b24f5bb6516bbb8fa",
+    "url": "/mylmap/static/js/2.8a0940e0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/mylmap/static/js/2.8a0940e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e04c4dc47e92b046eafe",
+    "url": "/mylmap/static/js/main.1afef3f7.chunk.js"
+  },
+  {
+    "revision": "d8de8e3fbb19899f0797",
+    "url": "/mylmap/static/js/runtime-main.88bc9efd.js"
+  }
+]);
